92d91a006fa3ead2d88f098908402611491be410
Gabriel da Silva Sousa <g.silva13455@gmail.com>
2020-08-28 21:02:27 -0300

    Pass term from child to parent component and API Youtube request

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,13 +6,17 @@ class SearchBar extends React.Component{
     onInputChange = (event) => {
         const text = event.target.value;
         this.setState({ term: text });
-        console.log(text);
+    }
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.onFormSubmit(this.state.term);
     }
 
     render(){
         return(
             <div className="search-bar ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Video Search</label>
                         <input type="text" value={this.state.term} onChange={this.onInputChange}></input>
